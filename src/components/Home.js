fce6255c8ab037d082e97d714258f3ef44c6355b
sumonmitrasm <sumonmitrasm@gmail.com>
2024-02-18 18:07:20 +0600

    route problrm solve

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Switch, Route, Link } from 'react-router-dom';
+ class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Ami home Component</h1>
+      </div>
+    )
+  }
+  
+}
+export default Home
